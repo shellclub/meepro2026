e58e4578b585665764a4b1ae334133192fccf9ff
Wirote Kaewroung <shellclub@gmail.com>
2026-03-05 10:43:34 +0700

    chore: skip type checks in prod build and increase ssh-action timeout to 40m

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
     // Uncomment when add value for NEXT_PUBLIC_PATH in .env.production or .env.development
     // basePath: process.env.NEXT_PUBLIC_PATH,
     trailingSlash: true,
